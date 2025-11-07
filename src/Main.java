926ed1c4f92532e86eb580efda506cb0d4fc99f0
titus-79 <rodolpheleg79@gmail.com>
2025-11-07 17:18:41 +0100

    feat : add loop on main menu

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,32 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        boolean continuer = true;
 
-        System.out.println("=== TODO LIST ===");
-        System.out.println("1. Ajouter une tâche");
-        System.out.println("0. Quitter");
-        System.out.print("Choisissez une option: ");
-        int choice = scanner.nextInt();
-        System.out.println("Vous avez choisi l'option: " + choice);
+        while (continuer) {
+            afficherMenu();
+            int choix = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choix) {
+                case 1:
+                    System.out.println("Fonctionnalité à venir...");
+                    break;
+                case 0:
+                    continuer = false;
+                    System.out.println("Au revoir!");
+                    break;
+                default:
+                    System.out.println("Option invalide, veuillez réessayer.");
+            }
+        }
         scanner.close();
+  
+    }
+    private static void afficherMenu() {
+    System.out.println("=== TODO LIST ===");
+    System.out.println("1. Ajouter une tâche");
+    System.out.println("0. Quitter");
+    System.out.print("Choisissez une option: ");
     }
 }
